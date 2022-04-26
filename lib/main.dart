d7f1ebce0fd55314fcb56d153fb28632d89121b5
ridhampokal <ridham1092@icloud.com>
2022-04-26 19:36:58 +0530

    demo commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+const TextStyle _textStyle = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 2,
+  fontStyle: FontStyle.italic,
+);
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  List pages = const [
+    Text('eco', style: _textStyle),
+    Text('home', style: _textStyle),
+    Text('person', style: _textStyle),
+    Text('video', style: _textStyle),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        title: const Text('NavigationBar Demo'),
+      ),
+      body: Center(
+        child: pages[_currentIndex],
+      ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: Colors.teal.withOpacity(0.5),
+          labelTextStyle: MaterialStateProperty.all(
+            const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        child: NavigationBar(
+          height: 60,
+          backgroundColor: Colors.white,
+          animationDuration: const Duration(seconds: 1),
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          selectedIndex: _currentIndex,
+          onDestinationSelected: (int newIndex) {
+            setState(() {
+              _currentIndex = newIndex;
+            });
+          },
+          destinations: const [
+            NavigationDestination(
+              selectedIcon: Icon(Icons.eco),
+              icon: Icon(Icons.eco_outlined),
+              label: 'eco',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'home',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.person),
+              icon: Icon(Icons.person_outlined),
+              label: 'person',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.video_camera_back),
+              icon: Icon(Icons.video_camera_back_outlined),
+              label: 'video',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
